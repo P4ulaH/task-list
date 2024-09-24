93c1a4ce9123de94753d6d146abeae9a66e22694
P4ulaH <149234751+P4ulaH@users.noreply.github.com>
2024-09-24 08:59:03 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,88 @@
+<?php
+
+use App\Http\Requests\TaskRequest;
+use Illuminate\Support\Facades\Route;
+use App\Models\Task;
+
+
+// Redirect to tasks index
+Route::get('/', function() {
+  return redirect() -> route('tasks.index');
+
+});
+
+// Route to list all tasks
+Route::get('/tasks', function() {
+  return view('index', [
+    'tasks' => Task::latest()->paginate(20) 
+  ]);
+})->name('tasks.index');
+
+// Route to create a task
+Route::view('/tasks/create', 'create')
+->name('tasks.create');
+
+// Route to edit a task
+Route::get('/tasks/{task}/edit', function (Task $task) {
+  return view('edit', [
+    'task' => $task
+  ]);
+}) ->name('tasks.edit');
+
+// Route to show a task
+Route::get('/tasks/{task}', function (Task $task) {
+  return view('show', [
+    'task' => $task
+  ]);
+}) ->name('tasks.show');
+
+Route::post('/tasks', function(TaskRequest $request){
+  
+  $task = Task::create($request->validated());
+
+  // Flash success message to the session
+  return redirect()->route('tasks.show', ['task' => $task->id])
+                   ->with('success', 'Task created successfully.');
+})->name('tasks.store');
+
+Route::put('/tasks/{task}', function(Task $task, TaskRequest $request){
+
+  $task->update($request->validated());
+
+  // Flash success message to the session
+  return redirect()->route('tasks.show', ['task' => $task->id])
+                   ->with('success', 'Task updated successfully.');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function(Task $task){
+  $task->delete();
+
+  // Flash success message to the session
+  return redirect()->route('tasks.index')
+                   ->with('success', 'Task deleted successfully.');
+})->name('tasks.destroy');
+
+Route::put('/tasks/{task}/toggle-complete', function(Task $task){
+  $task->toggleComplete();
+
+  // Flash success message to the session
+  return redirect()->back()->with('success', 'Task completed successfully.');
+})->name('tasks.toggle-complete');
+
+/*
+Route::get('/xxx', function () {
+    return 'Hello';
+}) ->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect() ->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello, ' . $name . '!';
+});
+*/
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
